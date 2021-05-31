a1bc531dfe64d3b36a7872ed21562db88bd7b423
Angel <gutierrezramosjoseangel@gmail.com>
2021-05-31 00:44:34 -0500

    Set data type on the response HTTP

--- src/hooks/usePokemonPaginated.tsx
+++ src/hooks/usePokemonPaginated.tsx
@@ -1,15 +1,26 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { pokemonApi } from "../api/pokemonApi";
+import { PokemonPaginatedResponse, Result, SimplePokemon } from '../interface/pokemonInterfaces';
 
 export const usePokemonPaginated = () => {
+    const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>([]);
     const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?offset=20&limit=40');
     
     const loadPokemons = async() => {
-        const response = await pokemonApi.get(nextPageUrl.current);
-        console.log(response);
+        const response = await pokemonApi.get<PokemonPaginatedResponse>(nextPageUrl.current);
+        nextPageUrl.current = response.data.next;
+        mapPokemonList(response.data.results);
+    }
+
+    const mapPokemonList = (pokemonList: Result[]) => {
+        pokemonList.forEach( pokemon => console.log(pokemon.name));
     }
 
     useEffect(() => {
         loadPokemons();
     }, []);
+
+    return { 
+        simplePokemonList
+    }
 }
---
+++ src/interface/pokemonInterfaces.ts
@@ -0,0 +1,18 @@
+export interface PokemonPaginatedResponse {
+    count: number;
+    next: string;
+    previous: string;
+    results: Result[];
+}
+
+export interface Result {
+    name: string;
+    url: string;
+}
+
+export interface SimplePokemon {
+    id: string;
+    name: string;
+    picture: string;
+    color?: string;
+}
