0b1c042e613d53bc0cd23e42f05a0ee1a9951936
Angel <gutierrezramosjoseangel@gmail.com>
2021-06-01 17:17:33 -0500

    Map Pokemons into a SimplePokemonArray

--- src/hooks/usePokemonPaginated.tsx
+++ src/hooks/usePokemonPaginated.tsx
@@ -3,17 +3,31 @@ import { pokemonApi } from "../api/pokemonApi";
 import { PokemonPaginatedResponse, Result, SimplePokemon } from '../interface/pokemonInterfaces';
 
 export const usePokemonPaginated = () => {
+    const [isLoading, setIsLoading] = useState(true);
     const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>([]);
-    const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?offset=20&limit=40');
+    const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?&limit=40');
     
     const loadPokemons = async() => {
+        setIsLoading(true);
+
         const response = await pokemonApi.get<PokemonPaginatedResponse>(nextPageUrl.current);
         nextPageUrl.current = response.data.next;
+
         mapPokemonList(response.data.results);
     }
 
     const mapPokemonList = (pokemonList: Result[]) => {
-        pokemonList.forEach( pokemon => console.log(pokemon.name));
+        const newPokemonList: SimplePokemon[] = pokemonList.map(({ name, url }) => {
+            //https://pokeapi.co/api/v2/pokemon/21/ 
+            const urlParts = url.split('/');
+            const id = urlParts[urlParts.length - 2];
+            const picture = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+            
+            return { id, picture, name };
+        });
+
+        setSimplePokemonList([...simplePokemonList, ...newPokemonList]);
+        setIsLoading(false);
     }
 
     useEffect(() => {
@@ -21,6 +35,8 @@ export const usePokemonPaginated = () => {
     }, []);
 
     return { 
-        simplePokemonList
+        isLoading,
+        simplePokemonList,
+        loadPokemons
     }
 }
