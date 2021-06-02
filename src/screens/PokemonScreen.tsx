e8a184a104a28fd0eb105a29419cb0507ff50da6
Angel <gutierrezramosjoseangel@gmail.com>
2021-06-02 01:03:53 -0500

    Work progress on the pokemon details screen

--- src/screens/PokemonScreen.tsx
+++ src/screens/PokemonScreen.tsx
@@ -1,16 +1,93 @@
 import React from 'react';
 import { StackScreenProps } from '@react-navigation/stack';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { RootStackParams } from '../navigator/StackNavigation';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { FadeInImage } from '../components/FadeInImage';
 
 interface Props extends StackScreenProps<RootStackParams, 'PokemonDetails'>{};
 
 export const PokemonScreen = ( {navigation, route}: Props ) => {
     const { simplePokemon, color } = route.params;
+    const { id, name, picture } = simplePokemon;
+    const { top } = useSafeAreaInsets();
 
     return (
         <View>
-            <Text style={{ color }}>{ simplePokemon.name } - { color }</Text>
+            {/* Header */}
+            <View style={{ 
+                ...styles.headerContainer,
+                backgroundColor: color
+            }}>
+                <TouchableOpacity 
+                    onPress={() => navigation.pop() }
+                    activeOpacity={0.8}
+                    style={{
+                        ...styles.backButton,
+                        top: top + 10
+                    }}
+                >
+                    <Icon 
+                        name='arrow-back-outline' 
+                        color='white' 
+                        size={35} 
+                    />
+                </TouchableOpacity>
+
+                {/* Pokemon Name */}
+                <Text style={{
+                    ...styles.pokemonName,
+                    top: top + 40
+                }}>
+                    { name + '\n' } #{ id }
+                </Text>
+
+                {/* Pokebola blanca */}
+                <Image 
+                    source={ require('../assets/pokebola-blanca.png') } 
+                    style={ styles.pokebola }
+                />
+
+                {/* Pokemon Image */}
+                <FadeInImage 
+                    uri={ picture }
+                    style={ styles.pokemonImage }
+                />
+
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    headerContainer: {
+        height: 370, 
+        zIndex: 999,
+        alignItems: 'center',
+        borderBottomRightRadius: 1000,
+        borderBottomLeftRadius: 1000
+    },
+    backButton: {
+        position: 'absolute',
+        left: 20
+    },
+    pokemonName: {
+        color: 'white',
+        fontSize: 40,
+        alignSelf: 'flex-start',
+        left: 20
+    },
+    pokebola: {
+        width: 250,
+        height: 250,
+        bottom: -20,
+        opacity: 0.7
+    },
+    pokemonImage: {
+        width: 250,
+        height: 250,
+        position: 'absolute',
+        bottom: -15
+    }
+});
