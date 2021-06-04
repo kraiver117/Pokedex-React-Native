0a18836ffbc5aa414eb8ac9b64773f32e9d9daf5
Angel <gutierrezramosjoseangel@gmail.com>
2021-06-03 23:49:25 -0500

    Weight, abilities, moves and sprites added to pokemon details screen

--- src/components/PokemonDetails.tsx
+++ src/components/PokemonDetails.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { FullPokemon } from '../interface/pokemonInterfaces';
+import { FadeInImage } from './FadeInImage';
 
 interface Props {
     pokemon: FullPokemon;
@@ -9,10 +10,13 @@ interface Props {
 
 export const PokemonDetails = ({ pokemon }: Props ) => {
     return (
-        <ScrollView style={{
+        <ScrollView 
+        showsVerticalScrollIndicator={ false }
+            style={{
             ...StyleSheet.absoluteFillObject
-        }}>
-            {/* Types */}
+            }}
+        >
+            {/* Types and weight */}
             <View style={{
                 ...styles.container,
                 marginTop: 390
@@ -30,15 +34,77 @@ export const PokemonDetails = ({ pokemon }: Props ) => {
                         ))
                     }
                 </View>
+
+                {/* Weight */}
+                <Text style={ styles.title }>Peso</Text>
+                <Text style={ styles.regularText }>{ pokemon.weight }kg</Text>
+
             </View>
 
             {/* Sprites */}
-            <View style={{ 
-                ...styles.container, 
-                marginTop: 20 
-            }}>
+            <View style={ styles.container }>
                 <Text style={ styles.title }>Sprites</Text>
+
+                <ScrollView
+                    showsHorizontalScrollIndicator={ false }
+                    horizontal={ true }
+                >
+                    <FadeInImage 
+                        uri={ pokemon.sprites.front_default }
+                        style={ styles.basicSprite }
+                    />
+
+                    <FadeInImage 
+                        uri={ pokemon.sprites.back_default }
+                        style={ styles.basicSprite }
+                    />
+
+                    <FadeInImage 
+                        uri={ pokemon.sprites.front_shiny }
+                        style={ styles.basicSprite }
+                    />
+
+                    <FadeInImage 
+                        uri={ pokemon.sprites.back_shiny }
+                        style={ styles.basicSprite }
+                    />
+                </ScrollView>
             </View>
+
+            {/* Abilities */}
+            <View style={ styles.container }>
+                <Text style={ styles.title }>Habilidades base</Text>
+                <View style={{ flexDirection: 'row'}}>
+                    {
+                        pokemon.abilities.map( ({ability}) => (
+                            <Text
+                                style={{ ...styles.regularText, marginRight: 10 }}
+                                key={ ability.name }
+                            >
+                                { ability.name }
+                            </Text>
+                        ))
+                    }
+                </View>
+            </View>
+
+            {/* Moves */}
+            <View style={ styles.container }>
+                <Text style={ styles.title }>Movimientos</Text>
+                <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+                    {
+                        pokemon.moves.map( ({move}) => (
+                            <Text
+                                style={{ ...styles.regularText, marginRight: 10 }}
+                                key={ move.name }
+                            >
+                                { move.name }
+                            </Text>
+                        ))
+                    }
+                </View>
+            </View>
+            
         </ScrollView>
     );
 }
@@ -49,9 +115,14 @@ const styles = StyleSheet.create({
     },
     title: {
         fontSize: 22,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        marginTop: 20 
     },
     regularText: {
         fontSize: 19
+    },
+    basicSprite: {
+        width: 100,
+        height: 100
     }
 });
